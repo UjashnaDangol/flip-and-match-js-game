9de3daf07cf69d5f7d3048d0ca3c7580550b015b
UjashnaDangol <ujashnadangol2004@gmail.com>
2025-05-29 17:52:40 +0545

    shuffle array function added

--- flip_and_match.js
+++ flip_and_match.js
@@ -28,23 +28,42 @@ const animals =[
   }
 ]
 
+function shuffle(array){
+ for (let i = array.length -1 ; i>0 ; i--){
+  
+ let j = Math.floor(Math.random()*(i+1));
+
+ let temp ;
+
+temp =array[i];
+ array[i]=array[j];
+ array[j]= temp;
+
+}
+ return array;
+}
 window.addEventListener("DOMContentLoaded", function () {
 document.getElementById("shuffle").addEventListener('click',function shuffleCards(){
    let imageContainer = document.getElementById("imageContainer");
 
   imageContainer.innerHTML= "";
-for (let i = 0 ; i < animals.length ; i++){
-  
- 
-  let img = document.createElement("img");
-  img.src = animals[i].images;
-  img.id = animals[i].id;
+
+  const doubledAnimals= [...animals,...animals];
+const shuffledarray= shuffle(doubledAnimals);
+
+shuffledarray.forEach(animal=>{
+let img = document.createElement("img");
+  img.src = animal.images;
+  img.id = animal.id;
   img.style.width= "100px";
   img.style.margin= "80px";
   
 
   imageContainer.appendChild(img);
+});
+  
 }
-});
+);
+}
+);
 
-});
