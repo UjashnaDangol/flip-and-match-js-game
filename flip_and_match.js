c1b0c89e3fdc20a7e9f35b17f78f739fa1e08925
UjashnaDangol <ujashnadangol2004@gmail.com>
2025-05-29 23:41:06 +0545

    flip effect added

--- flip_and_match.js
+++ flip_and_match.js
@@ -42,28 +42,60 @@ temp =array[i];
 }
  return array;
 }
+
+
 window.addEventListener("DOMContentLoaded", function () {
 document.getElementById("shuffle").addEventListener('click',function shuffleCards(){
    let imageContainer = document.getElementById("imageContainer");
 
   imageContainer.innerHTML= "";
 
-  const doubledAnimals= [...animals,...animals];
+const doubledAnimals= [...animals,...animals];
 const shuffledarray= shuffle(doubledAnimals);
 
 shuffledarray.forEach(animal=>{
-let img = document.createElement("img");
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let cardInner = document.createElement("div");
+  cardInner.classList.add("card-inner");
+
+  let cardFront = document.createElement("div");
+  cardFront.classList.add("card-front");
+
+  let img = document.createElement("img");
   img.src = animal.images;
   img.id = animal.id;
-  img.style.width= "100px";
-  img.style.margin= "80px";
+  img.classList.add("animal-img");
+  img.style.width = "100%";
+  img.style.height = "100%";
+  img.style.objectFit = "cover";
+
+  cardFront.appendChild(img);
+
+  let cardBack=document.createElement("div");
+  cardBack.classList.add("card-back");
+
+  cardInner.appendChild(cardFront);
+  cardInner.appendChild(cardBack);
+  card.appendChild(cardInner);
   
 
-  imageContainer.appendChild(img);
+  imageContainer.appendChild(card);
 });
   
 }
 );
 }
 );
+
+
+document.getElementById("imageContainer").addEventListener('click',function(e){
+
+  let card= e.target.closest(".card");
+  if(card){
+    card.classList.toggle("flipped");
+  }
+});
 
